1ab6d06d5b7210059e8ee119ac9645907fc376f6
blithe-hacker-0535 <sadiebenn1@gmail.com>
2019-02-18 23:17:42 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,11 @@
 class Hash
-  def keys_of(arguments)
+  def keys_of(*arguments)
     # code goes here
+    arr = []
+    hash.each do |key, value|
+      if arguments.include?(value)
+        arr << key
+      end
+    end
   end
 end
